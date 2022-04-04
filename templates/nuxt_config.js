c36d1020a8f5b9db8513d1c4e5293a14e8060003
volker <volker.jaenisch@inqbus.de>
2022-04-04 17:53:02 +0200

    Facetts clickable

--- templates/nuxt_config.js
+++ templates/nuxt_config.js
@@ -2,6 +2,7 @@
 
 export const SOLR_SELECT_URI='http://flask.datenadler.lan/solr_search';
 export const SOLR_SUGGEST_URI='http://flask.datenadler.lan/solr_suggest';
+export const SOLR_PICK_URI='http://flask.datenadler.lan/solr_pick';
 export const BASE_URL = 'https://backend.datenadler.lan/@search?fullobjects=1';
 export const MY_URL = 'https://datenadler.lan'
 export const FLASK_URL_MESSAGE = 'http://flask.datenadler.lan/send_email'
