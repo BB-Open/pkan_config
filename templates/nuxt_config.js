0f85cd9a0e42a7e19deda7d79f80ce092441ffa4
sandra <sandra.rum@inqbus.de>
2022-05-04 09:27:01 +0200

    New Configs for Plone Caching

--- templates/nuxt_config.js
+++ templates/nuxt_config.js
@@ -3,7 +3,6 @@
 export const SOLR_SELECT_URI='http://flask.datenadler.lan/solr_search';
 export const SOLR_SUGGEST_URI='http://flask.datenadler.lan/solr_suggest';
 export const SOLR_PICK_URI='http://flask.datenadler.lan/solr_pick';
-export const BASE_URL = 'https://backend.datenadler.lan/@search?fullobjects=1';
 export const MY_URL = 'https://datenadler.lan'
 export const FLASK_URL_MESSAGE = 'http://flask.datenadler.lan/send_email'
 export const FLASK_URL_PLONE = 'http://flask.datenadler.lan/request_plone'
