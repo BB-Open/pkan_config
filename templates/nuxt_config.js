fda65765bbf2b743e7daf575c1f5e48eed9f4411
sandra <sandra.rum@inqbus.de>
2022-11-07 14:02:56 +0100

    new config field

--- templates/nuxt_config.js
+++ templates/nuxt_config.js
@@ -3,6 +3,8 @@
 export const SOLR_SELECT_URI='http://flask.datenadler.lan/solr_search';
 export const SOLR_SUGGEST_URI='http://flask.datenadler.lan/solr_suggest';
 export const SOLR_PICK_URI='http://flask.datenadler.lan/solr_pick';
+export const SOLR_ROULETTE_URI='http://flask.datenadler.lan/solr_roulette'
+
 export const MY_URL = 'https://datenadler.lan'
 export const FLASK_URL_MESSAGE = 'http://flask.datenadler.lan/send_email'
 export const FLASK_URL_PLONE = 'http://flask.datenadler.lan/request_plone'
